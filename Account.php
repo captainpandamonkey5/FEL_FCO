7d2d3a652d2639d58dedb559238186e27679aec9
Bryyyyy <bryancalulo@gmail.com>
2024-03-22 21:27:06 +0800

    updated php files

--- Account.php
+++ Account.php
@@ -1,32 +1,50 @@
 <html>
-    <head>
-		
-		<title>AJC Bike Shop MIS</title>
-		<link rel="stylesheet" href="css/main_style.css">
-		<link rel="stylesheet" href="css/report_style.css">
-    </head>
-    <body>
+  <head>
+    <title>AJC Bike Shop MIS</title>
+    <link rel="stylesheet" href="css/main_style.css" />
+    <link rel="stylesheet" href="css/account_styles.css" />
+    <style>
+      #acc_btn {
+        background-color: rgb(41, 41, 41);
+        color: white;
+      }
+      #acc_btn path,
+      #acc_btn svg,
+      #acc_btn rect {
+        fill: white;
+      }
+      .main_section * {
+        box-sizing: border-box;
+        font-family: Tahoma, "Trebuschet MS", sans-serif;
+      }
+      html,
+      body,
+      .wrapper {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- for modifies account settings-->
 
-        <!-- lists all report about transactions or order -->
-		
-		<div class="nav_bar">
+    <div class="nav_bar">
 			<ul class="main_nav">
-				<a href = "POS.html">
+				<a href = "POS.php">
 					<li id="pos_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 32 32" id="swipe-machine"><path fill="#231f20" d="M19.12 7H4.26a2.05 2.05 0 0 0-2 2v20.9A2.06 2.06 0 0 0 4.26 32H19.12a2.06 2.06 0 0 0 2.06-2.06V9A2.05 2.05 0 0 0 19.12 7zM7.62 28.72H5.73v-1.9H7.62zm0-4.71H5.73v-1.9H7.62zm0-4.71H5.73V17.4H7.62zm5 9.42H10.74v-1.9h1.91zm0-4.71H10.74v-1.9h1.91zm0-4.71H10.74V17.4h1.91zm5 9.42h-1.9v-1.9h1.9zm0-4.71h-1.9v-1.9h1.9zm0-4.71h-1.9V17.4h1.9zm.65-6a.34.34 0 0 1-.33.34H5.41a.34.34 0 0 1-.33-.34V10.59a.33.33 0 0 1 .33-.33H18a.33.33 0 0 1 .33.33zM28.45 10.4H28V28.58h.5a1.34 1.34 0 0 0 1.34-1.34V11.75A1.34 1.34 0 0 0 28.45 10.4z"></path><rect width="2.93" height="18.19" x="22.11" y="10.4" fill="#231f20"></rect><rect width="1.26" height="18.19" x="25.86" y="10.4" fill="#231f20"></rect><path fill="#231f20" d="M20.61 1.79A1.78 1.78 0 0 0 20.09.52 1.82 1.82 0 0 0 18.82 0 1.79 1.79 0 0 0 17 1.79V2.85h3.58zM16.21 1.79a1.52 1.52 0 0 1 0-.29 1.36 1.36 0 0 1 0-.21l-.1 0 .12 0A1 1 0 0 1 16.33 1 .86.86 0 0 1 16.4.8a2.39 2.39 0 0 1 .29-.52 1 1 0 0 1 .08-.11L16.91 0H8.81A1.63 1.63 0 0 0 7.18 1.62V6.06h9z"></path></svg><p>Order</p>
 					</li>
 				</a>
-				<a href = "Inventory.html">
+				<a href = "Inventory.php">
 					<li id="inv_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" id="box"><path d="M18.399 2H1.6c-.332 0-.6.267-.6.6V5h18V2.6a.6.6 0 0 0-.601-.6zM2 16.6c0 .77.629 1.4 1.399 1.4h13.2c.77 0 1.4-.631 1.4-1.4V6H2v10.6zM7 8h6v2H7V8z"></path></svg><p>Inventory</p>
 					</li>
 				</a>
-				<a href = "OrderReport.html">
+				<a href = "OrderReport.php">
 					<li id="report_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 64" id="business-analysis"><path fill="#010101" d="M41.56,5.23h-4.1a1,1,0,0,1-1-1,4.43,4.43,0,0,0-8.85,0,1,1,0,0,1-1,1h-4.1L20.62,12.1H43.38ZM32,9.31a3.26,3.26,0,1,1,3.26-3.26A3.26,3.26,0,0,1,32,9.31Z"></path><path fill="#010101" d="M53.26 9.59h-8.4l.86 3.27a1 1 0 0 1-.17.9 1.06 1.06 0 0 1-.83.4H19.28a1 1 0 0 1-1-1.3l.86-3.27h-8.4a1 1 0 0 0-1 1V63a1 1 0 0 0 1 1H53.26a1 1 0 0 0 1-1V10.62A1 1 0 0 0 53.26 9.59zM23.59 60.88a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V53.23a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zM40.8 36.38a1 1 0 1 1 .63-2l3.4 1.08h0a1 1 0 0 1 .62.54 1 1 0 0 1 0 .8v0l-1.34 3.31a1 1 0 0 1-1 .65 1.08 1.08 0 0 1-.38-.08 1 1 0 0 1-.57-1.35l.36-.87L19.93 49.18a1.15 1.15 0 0 1-.44.09 1 1 0 0 1-.44-2L41.69 36.66zm-5.25 24.5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V48.07a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zm12 0a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-18a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zm.4-32.42H16.09a1 1 0 0 1 0-2.07H47.91a1 1 0 0 1 0 2.07zm0-6.89H16.09a1 1 0 1 1 0-2.06H47.91a1 1 0 1 1 0 2.06zM32 4.54a1.51 1.51 0 1 0 1.5 1.51A1.51 1.51 0 0 0 32 4.54z"></path><rect width="2.96" height="5.58" x="18.57" y="54.27" fill="#010101"></rect><rect width="2.96" height="10.75" x="30.52" y="49.1" fill="#010101"></rect><rect width="2.97" height="15.91" x="42.47" y="43.94" fill="#010101"></rect></svg><p>Report</p>
 					</li>
 				</a>
-				<a href = "Account.html">
+				<a href = "Account.php">
 					<li id="acc_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="Accountant"><path fill="#000000" d="m43.41 38.69-.44-.16c-1.74-.62-2.7-2.08-3.25-3.47 5.33-3.4 9.14-10.07 9.14-17.37C48.87 7.61 41.61 0 32.01 0 22.4 0 15.14 7.61 15.14 17.7c0 7.29 3.8 13.96 9.13 17.36-.69 1.78-1.78 2.95-3.25 3.48l-.44.16c-7.17 2.57-15.29 5.49-16.4 17.02-.06.59.25 1.14.76 1.41C13.69 61.69 22.69 64 31.71 64c9.01 0 18.21-2.31 27.31-6.86.53-.27.84-.83.78-1.42-1.1-11.53-9.22-14.45-16.39-17.03zM21.12 15.17c3.13.75 12.48 2.32 19.96-2.92.8 1.18 2.32 3.91 3.28 8.49h-.08c-.24-.57-.66-1.17-1.25-1.76-.59-.59-1.41-.93-2.23-.93H22.83c-.84 0-1.63.33-2.23.93-.25.25-.45.5-.64.74.43-1.28.86-2.82 1.16-4.55zm-2.33 7.75c.03 0 .05.01.07.01h.46c.37 1.34 1.4 4.21 3.62 5.25.57.27 1.16.4 1.78.4.92 0 1.9-.3 2.92-.89 2.39-1.39 3.69-3.02 3.86-4.86.08-.85-.1-1.72-.53-2.59h1.69c-.43.86-.61 1.73-.53 2.59.17 1.84 1.47 3.47 3.86 4.86 1.02.59 2 .89 2.93.89.61 0 1.21-.13 1.77-.4 2.16-1 3.19-3.66 3.62-5.25h.45c.05.32.09.64.14.98-2.19 6.05-7.35 10.65-12.88 10.65-5.86 0-11.27-5.11-13.23-11.64zm16.67 36.22L32 57.32l-3.46 1.82.66-3.85-2.8-2.73 3.87-.56L32 48.5l1.73 3.5 3.87.56-2.8 2.73.66 3.85zm-3.45-15.6a74.43 74.43 0 0 1-6.49-4.88c.52-.66.95-1.43 1.31-2.28 1.65.67 3.39 1.05 5.18 1.05 1.78 0 3.52-.38 5.16-1.04.36.86.79 1.63 1.31 2.29-2.69 2.28-5.37 4.12-6.47 4.86z" class="color292f64 svgShape"></path></svg><p>Account</p>
 					</li>
@@ -34,12 +52,12 @@
 			</ul>
 			
 			<ul class="misc_nav">
-				<a href = "Help.html">
+				<a href = "Help.php">
 					<li id="help_btn">
 						<p>Help</p>
 					</li>
 				</a>
-				<a href = "AboutUs.html">
+				<a href = "AboutUs.php">
 					<li id="info_btn">
 						<p>About Us</p>
 					</li>
@@ -47,58 +65,26 @@
 			</ul>
 		</div>
 
-		<div class="main_section">
-			<div class="report_header">
-				<div class="report_title">Reports</div>
-				<hr/>
-				<div class="report_nav">
-					<button id="orderreport_btn" onclick= "window.location.href='OrderReport.html'">Order History</button>
-					<button id="salesreport_btn" onclick= "window.location.href='SalesReport.html'">Sales Report</button>
-					<button id="stockreport_btn" onclick= "window.location.href='StockReport.html'">Stock History</button>
-				</div>
-			</div>
-			<div class="report_search">
-				<p>Stock History</p>
-				<input type="date" id = "dateSearch" name="dateSearch">
-				<input type="text" id = "idSearch" name="idSearch" placeholder="Search for Order No.">
-				<button id="search_btn"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" id="search"><path d="M46.599 40.236L36.054 29.691C37.89 26.718 39 23.25 39 19.5 39 8.73 30.27 0 19.5 0S0 8.73 0 19.5 8.73 39 19.5 39c3.75 0 7.218-1.11 10.188-2.943l10.548 10.545a4.501 4.501 0 0 0 6.363-6.366zM19.5 33C12.045 33 6 26.955 6 19.5S12.045 6 19.5 6 33 12.045 33 19.5 26.955 33 19.5 33z"></path></svg></button>
-			</div>
-			<div class="report_table"> 
-				<table cellspacing="0">
-					<tr>
-						<th>OrderNo</th>
-						<th>ProductOrdered</th>
-						<th>SupplierName</th>
-						<th>OrderDate</th>
-						<th>QuantityOrdered</th>
-						<th>CostPerUnit</th>
-						<th>TotalCost</th>
-					</tr>
-					<tr>
-						<td>ewggggweg</td>
-						<td>sdfsdfsdfsdfs</td>
-						<td>sdfsdf</td>
-						<td>ggsd</td>
-						<td>sadg asd</td>
-						<td>sadgas </td>
-						<td>sdag</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td></td>
-						<td> </td>
-						<td></td>
-						<td> </td>
-						<td> </td>
-						<td></td>
-					</tr>
-				</table>
-			</div>
-			<div class="report_table_footer"> 
-				<p>Showing XXX Orders</p>
-			</div>
-		</div>
-        
-    </body>
-
+    <div class="main_section">
+      <div class="accountSetting_Body">
+        <h2>Account Settings</h2>
+        <form class="security_form">
+          <div class="password_form">
+            <label for="password">Change Password</label>
+            <input type="password" placeholder="Old Password" />
+            <input type="password" placeholder="New Password" />
+            <button type="submit">Save Changes</button>
+          </div>
+          <div class="username_form">
+            <label for="username">Change Username</label>
+            <input type="text" placeholder="New Username" />
+            <button type="submit">Save Changes</button>
+          </div>
+        </form>
+      </div>
+      <div class="accountmngr_directory">
+        <a href="AccountMngr.php"> Account Manager </a>
+      </div>
+    </div>
+  </body>
 </html>
--- Inventory.php
+++ Inventory.php
@@ -3,30 +3,39 @@
 		
 		<title>AJC Bike Shop MIS</title>
 		<link rel="stylesheet" href="css/main_style.css">
-		<link rel="stylesheet" href="css/report_style.css">
+		<link rel="stylesheet" href="css/inv_style.css">
+		<style>
+            #inv_btn{
+                background-color: rgb(41, 41, 41);
+                color:white
+            }
+            #inv_btn path, #inv_btn svg,  #inv_btn rect{
+                fill:white;
+            }
+        </style>
     </head>
     <body>
 
-        <!-- lists all report about transactions or order -->
+        <!-- for MANAGING THE STOCKS, allows create read update delete -->
 		
 		<div class="nav_bar">
 			<ul class="main_nav">
-				<a href = "POS.html">
+				<a href = "POS.php">
 					<li id="pos_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 32 32" id="swipe-machine"><path fill="#231f20" d="M19.12 7H4.26a2.05 2.05 0 0 0-2 2v20.9A2.06 2.06 0 0 0 4.26 32H19.12a2.06 2.06 0 0 0 2.06-2.06V9A2.05 2.05 0 0 0 19.12 7zM7.62 28.72H5.73v-1.9H7.62zm0-4.71H5.73v-1.9H7.62zm0-4.71H5.73V17.4H7.62zm5 9.42H10.74v-1.9h1.91zm0-4.71H10.74v-1.9h1.91zm0-4.71H10.74V17.4h1.91zm5 9.42h-1.9v-1.9h1.9zm0-4.71h-1.9v-1.9h1.9zm0-4.71h-1.9V17.4h1.9zm.65-6a.34.34 0 0 1-.33.34H5.41a.34.34 0 0 1-.33-.34V10.59a.33.33 0 0 1 .33-.33H18a.33.33 0 0 1 .33.33zM28.45 10.4H28V28.58h.5a1.34 1.34 0 0 0 1.34-1.34V11.75A1.34 1.34 0 0 0 28.45 10.4z"></path><rect width="2.93" height="18.19" x="22.11" y="10.4" fill="#231f20"></rect><rect width="1.26" height="18.19" x="25.86" y="10.4" fill="#231f20"></rect><path fill="#231f20" d="M20.61 1.79A1.78 1.78 0 0 0 20.09.52 1.82 1.82 0 0 0 18.82 0 1.79 1.79 0 0 0 17 1.79V2.85h3.58zM16.21 1.79a1.52 1.52 0 0 1 0-.29 1.36 1.36 0 0 1 0-.21l-.1 0 .12 0A1 1 0 0 1 16.33 1 .86.86 0 0 1 16.4.8a2.39 2.39 0 0 1 .29-.52 1 1 0 0 1 .08-.11L16.91 0H8.81A1.63 1.63 0 0 0 7.18 1.62V6.06h9z"></path></svg><p>Order</p>
 					</li>
 				</a>
-				<a href = "Inventory.html">
+				<a href = "Inventory.php">
 					<li id="inv_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" id="box"><path d="M18.399 2H1.6c-.332 0-.6.267-.6.6V5h18V2.6a.6.6 0 0 0-.601-.6zM2 16.6c0 .77.629 1.4 1.399 1.4h13.2c.77 0 1.4-.631 1.4-1.4V6H2v10.6zM7 8h6v2H7V8z"></path></svg><p>Inventory</p>
 					</li>
 				</a>
-				<a href = "OrderReport.html">
+				<a href = "OrderReport.php">
 					<li id="report_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 64" id="business-analysis"><path fill="#010101" d="M41.56,5.23h-4.1a1,1,0,0,1-1-1,4.43,4.43,0,0,0-8.85,0,1,1,0,0,1-1,1h-4.1L20.62,12.1H43.38ZM32,9.31a3.26,3.26,0,1,1,3.26-3.26A3.26,3.26,0,0,1,32,9.31Z"></path><path fill="#010101" d="M53.26 9.59h-8.4l.86 3.27a1 1 0 0 1-.17.9 1.06 1.06 0 0 1-.83.4H19.28a1 1 0 0 1-1-1.3l.86-3.27h-8.4a1 1 0 0 0-1 1V63a1 1 0 0 0 1 1H53.26a1 1 0 0 0 1-1V10.62A1 1 0 0 0 53.26 9.59zM23.59 60.88a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V53.23a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zM40.8 36.38a1 1 0 1 1 .63-2l3.4 1.08h0a1 1 0 0 1 .62.54 1 1 0 0 1 0 .8v0l-1.34 3.31a1 1 0 0 1-1 .65 1.08 1.08 0 0 1-.38-.08 1 1 0 0 1-.57-1.35l.36-.87L19.93 49.18a1.15 1.15 0 0 1-.44.09 1 1 0 0 1-.44-2L41.69 36.66zm-5.25 24.5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V48.07a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zm12 0a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-18a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1zm.4-32.42H16.09a1 1 0 0 1 0-2.07H47.91a1 1 0 0 1 0 2.07zm0-6.89H16.09a1 1 0 1 1 0-2.06H47.91a1 1 0 1 1 0 2.06zM32 4.54a1.51 1.51 0 1 0 1.5 1.51A1.51 1.51 0 0 0 32 4.54z"></path><rect width="2.96" height="5.58" x="18.57" y="54.27" fill="#010101"></rect><rect width="2.96" height="10.75" x="30.52" y="49.1" fill="#010101"></rect><rect width="2.97" height="15.91" x="42.47" y="43.94" fill="#010101"></rect></svg><p>Report</p>
 					</li>
 				</a>
-				<a href = "Account.html">
+				<a href = "Account.php">
 					<li id="acc_btn">
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="Accountant"><path fill="#000000" d="m43.41 38.69-.44-.16c-1.74-.62-2.7-2.08-3.25-3.47 5.33-3.4 9.14-10.07 9.14-17.37C48.87 7.61 41.61 0 32.01 0 22.4 0 15.14 7.61 15.14 17.7c0 7.29 3.8 13.96 9.13 17.36-.69 1.78-1.78 2.95-3.25 3.48l-.44.16c-7.17 2.57-15.29 5.49-16.4 17.02-.06.59.25 1.14.76 1.41C13.69 61.69 22.69 64 31.71 64c9.01 0 18.21-2.31 27.31-6.86.53-.27.84-.83.78-1.42-1.1-11.53-9.22-14.45-16.39-17.03zM21.12 15.17c3.13.75 12.48 2.32 19.96-2.92.8 1.18 2.32 3.91 3.28 8.49h-.08c-.24-.57-.66-1.17-1.25-1.76-.59-.59-1.41-.93-2.23-.93H22.83c-.84 0-1.63.33-2.23.93-.25.25-.45.5-.64.74.43-1.28.86-2.82 1.16-4.55zm-2.33 7.75c.03 0 .05.01.07.01h.46c.37 1.34 1.4 4.21 3.62 5.25.57.27 1.16.4 1.78.4.92 0 1.9-.3 2.92-.89 2.39-1.39 3.69-3.02 3.86-4.86.08-.85-.1-1.72-.53-2.59h1.69c-.43.86-.61 1.73-.53 2.59.17 1.84 1.47 3.47 3.86 4.86 1.02.59 2 .89 2.93.89.61 0 1.21-.13 1.77-.4 2.16-1 3.19-3.66 3.62-5.25h.45c.05.32.09.64.14.98-2.19 6.05-7.35 10.65-12.88 10.65-5.86 0-11.27-5.11-13.23-11.64zm16.67 36.22L32 57.32l-3.46 1.82.66-3.85-2.8-2.73 3.87-.56L32 48.5l1.73 3.5 3.87.56-2.8 2.73.66 3.85zm-3.45-15.6a74.43 74.43 0 0 1-6.49-4.88c.52-.66.95-1.43 1.31-2.28 1.65.67 3.39 1.05 5.18 1.05 1.78 0 3.52-.38 5.16-1.04.36.86.79 1.63 1.31 2.29-2.69 2.28-5.37 4.12-6.47 4.86z" class="color292f64 svgShape"></path></svg><p>Account</p>
 					</li>
@@ -34,12 +43,12 @@
 			</ul>
 			
 			<ul class="misc_nav">
-				<a href = "Help.html">
+				<a href = "Help.php">
 					<li id="help_btn">
 						<p>Help</p>
 					</li>
 				</a>
-				<a href = "AboutUs.html">
+				<a href = "AboutUs.php">
 					<li id="info_btn">
 						<p>About Us</p>
 					</li>
@@ -47,52 +56,65 @@
 			</ul>
 		</div>
 
+
 		<div class="main_section">
-			<div class="report_header">
-				<div class="report_title">Reports</div>
-				<hr/>
-				<div class="report_nav">
-					<button id="orderreport_btn" onclick= "window.location.href='OrderReport.html'">Order History</button>
-					<button id="salesreport_btn" onclick= "window.location.href='SalesReport.html'">Sales Report</button>
-					<button id="stockreport_btn" onclick= "window.location.href='StockReport.html'">Stock History</button>
-				</div>
+
+			<div class="inv_misc">
+				<button id="addItem_btn">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="add"><g data-name="20. Add"><path d="M17 8a8.94 8.94 0 0 1 5 1.52V4a4 4 0 0 0-4-4H4a4 4 0 0 0-4 4v14a4 4 0 0 0 4 4h5.52A9 9 0 0 1 17 8Z"></path><path d="M17 10a7 7 0 1 0 7 7 7.008 7.008 0 0 0-7-7Zm2 8h-1v1a1 1 0 0 1-2 0v-1h-1a1 1 0 0 1 0-2h1v-1a1 1 0 0 1 2 0v1h1a1 1 0 0 1 0 2Z"></path></g></svg>
+				</button>
 			</div>
-			<div class="report_search">
-				<p>Sales Report</p>
-				<input type="date" id = "dateSearch" name="dateSearch">
-				<input type="text" id = "idSearch" name="idSearch" placeholder="Search for Sale No.">
-				<button id="search_btn"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" id="search"><path d="M46.599 40.236L36.054 29.691C37.89 26.718 39 23.25 39 19.5 39 8.73 30.27 0 19.5 0S0 8.73 0 19.5 8.73 39 19.5 39c3.75 0 7.218-1.11 10.188-2.943l10.548 10.545a4.501 4.501 0 0 0 6.363-6.366zM19.5 33C12.045 33 6 26.955 6 19.5S12.045 6 19.5 6 33 12.045 33 19.5 26.955 33 19.5 33z"></path></svg></button>
-			</div>
-			<div class="report_table"> 
-				<table cellspacing="0">
-					<tr>
-						<th>SalesNo</th>
-						<th>SaleDateByDay</th>
-						<th>Cash</th>
-						<th>Excess</th>
-						<th>Total</th>
-					</tr>
-					<tr>
-						<td>ewggggweg</td>
-						<td>sdfsdfsdfsdfs</td>
-						<td>sdfsdf</td>
-						<td>ggsd</td>
-						<td>sadg asd</td>
-					</tr>
-					<tr>
-						<td></td>
-						<td></td>
-						<td> </td>
-						<td></td>
-						<td> </td>
-					</tr>
+			<div class="inventory_section">
+				<table>
+							<tr>
+								<th>ProductID</th>
+								<th>Name </th> 
+								<th>Category </th> 
+								<th>Price</th>
+								<th>Description</th>
+								<th>Quantity</th>
+								<th>Actions</th>
+							</tr>
+							<tr>
+								<td><p>10001</p></td>
+								<td><p>Saddle X6 PRO++ Ultra</p>
+									<button >
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pencil"><path d="M8.661 19.113 3 21l1.887-5.661ZM20.386 7.388a2.1 2.1 0 0 0 0-2.965l-.809-.809a2.1 2.1 0 0 0-2.965 0L6.571 13.655l3.774 3.774Z"></path></svg>								</button>
+								</td>
+								<td><p>Saddle</p>
+									<button >
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pencil"><path d="M8.661 19.113 3 21l1.887-5.661ZM20.386 7.388a2.1 2.1 0 0 0 0-2.965l-.809-.809a2.1 2.1 0 0 0-2.965 0L6.571 13.655l3.774 3.774Z"></path></svg>								</button>
+								</td>
+								<td ><p>PHP 400.00</p>
+									<button>
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pencil"><path d="M8.661 19.113 3 21l1.887-5.661ZM20.386 7.388a2.1 2.1 0 0 0 0-2.965l-.809-.809a2.1 2.1 0 0 0-2.965 0L6.571 13.655l3.774 3.774Z"></path></svg>			
+									</button>	
+								</td>
+								<td>
+									<p>Lorem ipsum dolor is et at</p>
+								</td>
+								<td ><p>10</p>
+									<button id="decQty_btn">-</button>
+									<button id="addQty_btn">+</button>
+								</td>
+								<td>
+									<button >
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" id="delete"><path fill="#000" d="M15 3a1 1 0 0 1 1 1h2a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2h2a1 1 0 0 1 1-1h6Z"></path><path fill="#000" fill-rule="evenodd" d="M6 7h12v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V7Zm3.5 2a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0v-9a.5.5 0 0 0-.5-.5Zm5 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0v-9a.5.5 0 0 0-.5-.5Z" clip-rule="evenodd"></path></svg>								</button>	
+								</td>
+								
+							</tr>
+							
+
+							
+							
 				</table>
 			</div>
-			<div class="report_table_footer"> 
-				<p>Showing XXX Orders</p>
-			</div>
+		
 		</div>
-        
-    </body>
+
+	<script src="inventory.js">
+	</script>
+
+</body>
 
 </html>
